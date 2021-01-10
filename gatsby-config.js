395d7e705f3f7f0bfc76cb1efadfcaae2edc2086
Szymon Polaczy <polaczyszymon@gmail.com>
2021-01-10 21:21:35 +0100

    Adding neccessary meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,13 @@
 module.exports = {
   plugins: [
+    `gatsby-plugin-react-helmet`,
     "gatsby-plugin-emotion",
-    "gatsby-plugin-postcss"
+    "gatsby-plugin-postcss",
+    {
+      resolve: 'gatsby-plugin-html-attributes',
+      options: {
+        lang: 'en'
+      }
+    }
   ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 
 import devices from "./undraw_web_devices_ad58.svg";
 import world from "./undraw_connected_world_wuay.svg";
@@ -9,60 +10,67 @@ import contact from "./undraw_source_code_xx2e.svg";
 
 const Index = () => {
   return (
-    <main className="p-6 lg:p-0 box-content">
-      <header className="relative w-full h-screen text-center">
-        <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-7xl uppercase">
-          Software 
-          <span className="block lowercase text-3xl mt-4 text-gray-400">as simple as it can be</span>
-          <a href="mailto:polaczyszymon@gmail.com" title="Contact Me" className="text-2xl hover:underline">Contact Me</a>
-        </h1>
-        <a href="#what" title="See more" 
-          className="absolute bottom-12 w-max left-0 right-0 mx-auto block text-2xl 
-            text-gray-500 transform hover:scale-110 duration-200 transition-all"
-        >
-          See more
-        </a>
-      </header>
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Get Over Software</title>
+        <link rel="canonical" href="https://get-over.online"/>
+      </Helmet>
+      <main className="p-6 box-content space-y-64 text-center text-gray-700 uppercase text-4xl">
+        <header className="relative w-full h-screen ">
+          <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-7xl ">
+            Software 
+            <span className="block lowercase text-3xl mt-4 text-gray-400">as simple as it can be</span>
+            <a href="mailto:polaczyszymon@gmail.com" title="Contact Me" className="text-2xl hover:underline">Contact Me</a>
+          </h1>
+          <a href="#what" title="See more" 
+            className="absolute bottom-12 w-max left-0 right-0 mx-auto block text-2xl 
+              text-gray-500 transform hover:scale-110 duration-200 transition-all"
+          >
+            See more
+          </a>
+        </header>
 
-      <section className="grid xl:grid-cols-2 mt-56" id="what">
-        <article className="px-12">
-          <h2 className="text-4xl uppercase text-gray-700 text-center pb-8">Whatever the device is</h2>
-          <img src={devices} alt="Phone, Laptop, Big Screen" className="h-125 mx-auto"/>
-        </article>
+        <section className="grid xl:grid-cols-2" id="what">
+          <article>
+            <h2>Whatever the device is</h2>
+            <img src={devices} alt="Phone, Laptop, Big Screen" className="h-125 mx-auto"/>
+          </article>
 
-        <article className="px-12 mt-24 xl:mt-0">
-          <h2 className="text-4xl uppercase text-gray-700 text-center pb-8">Wherever you are</h2>
-          <img src={world} alt="Las Vegas, Chille, In the middle of the woods" className="h-125 mx-auto"/>
-        </article>
-      </section>
+          <article className="mt-24 xl:mt-0">
+            <h2>Wherever you are</h2>
+            <img src={world} alt="Las Vegas, Chille, In the middle of the woods" className="h-125 mx-auto"/>
+          </article>
+        </section>
 
-      <section className="mt-56 text-center">
-        <h2 className="text-4xl uppercase text-gray-700 pb-8">Let's keep your software projects simple</h2>
-        <img src={simple_innovative} alt="Simple and Inovative" className="w-full max-w-screen-lg mx-auto"/>
-      </section>
+        <section>
+          <h2>Let's keep your software projects simple</h2>
+          <img src={simple_innovative} alt="Simple and Inovative" className="w-full max-w-screen-lg mx-auto"/>
+        </section>
 
-      <section className="mt-56 text-center">
-        <h2 className="text-4xl uppercase text-gray-700 pb-8">While</h2>
-      </section>
+        <section>
+          <h2>While</h2>
+        </section>
 
-      <section className="grid xl:grid-cols-2 mt-56 text-center">
-        <article>
-          <h2 className="text-4xl uppercase text-gray-700 pb-8">Letting you stand out</h2>
-          <img src={stand_out} alt="" className="h-125 px-12 mx-auto"/>
-        </article>
+        <section className="grid xl:grid-cols-2">
+          <article>
+            <h2>Letting you stand out</h2>
+            <img src={stand_out} alt="" className="h-125 mx-auto"/>
+          </article>
 
-        <article className="mt-24 xl:mt-0">
-          <h2 className="text-4xl uppercase text-gray-700 pb-8">And getting your users absorbed</h2>
-          <img src={absorbed} alt="" className="h-125 px-12 mx-auto"/>
-        </article>
-      </section>
+          <article className="mt-24 xl:mt-0">
+            <h2>And getting your users absorbed</h2>
+            <img src={absorbed} alt="" className="h-125 mx-auto"/>
+          </article>
+        </section>
 
-      <section className="mt-56 text-center mb-36 relative w-full lg:w-max mx-auto">
-        <h2 className="text-4xl uppercase text-gray-700 pb-8">let's get in contact</h2>
-        <img src={contact} alt="" className="w-full max-w-screen-lg mx-auto"/>
-        <a href="mailto:polaczyszymon@gmail.com" className="absolute bottom-32 right-24 text-3xl hover:underline">My mail is right here</a>
-      </section>
-    </main>
+        <section className="mb-36 relative w-full lg:w-max mx-auto">
+          <h2>let's get in contact</h2>
+          <img src={contact} alt="" className="w-full max-w-screen-lg mx-auto"/>
+          <a href="mailto:polaczyszymon@gmail.com" className="md:absolute md:bottom-32 md:right-24 block mt-6 md:mt-0 text-3xl hover:underline">My mail is right here</a>
+        </section>
+      </main>
+    </>
   )
 }
 

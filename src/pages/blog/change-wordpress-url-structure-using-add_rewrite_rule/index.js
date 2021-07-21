39653a7748097d26ef6176956a3b4fcf7093e7b8
Szymon <thomas_frey@tutanota.com>
2021-07-21 20:12:33 +0200

    Headline 3 styles and Blogpost text work

--- src/pages/blog/change-wordpress-url-structure-using-add_rewrite_rule/index.js
+++ src/pages/blog/change-wordpress-url-structure-using-add_rewrite_rule/index.js
@@ -21,7 +21,7 @@ const Index = () => {
         <section className="text-left md:px-16 xl:px-32 2xl:px-48" id="what">
           <h2 className="blog-headline-2">Why would you do this?</h2>
           <p>I can give you a couple of examples where have I used this.</p>
-          <ul>
+          <ul className="blog-list">
             <li>I wanted to have kind of tree like structure of my term links (Basically what I mean is I would have a parent/ => parent/child/ => grandparent/parent/child/</li>
             <li>I had to create search filter which would use posts names in the search url - That didnt work because Wordpress tries to help the user find what they're looking for and it redirected these search url to the posts that were in it's url</li>
           </ul>
@@ -38,28 +38,19 @@ const Index = () => {
           <div className="code">
             <code dangerouslySetInnerHTML={{__html: `add_rewrite_rule('cities/([a-z0-9-_]+)/([a-z0-9-_]+), 'index.php?cities=$matches[2], 'top);`}}></code>
           </div>
+          <p>And this is how you should check for every allowed character in the slug <code>([a-z0-9-_]+)</code></p>
 
           <hr></hr>
 
-          <div className="code">
-            <code 
-              dangerouslySetInnerHTML={{__html: `
-                add_action('init', function () &lbrace;<br>
-                &nbsp;add_rewrite_rule('inwestycje/([a-z0-9-_]+)/([a-z0-9-_]+)', 'index.php?inwestycje=$matches[2]', 'top');<br><bbr></bbr>
-      
-                &nbsp;$cities = get_categories('taxonomy=inwestycje');<br>
-                &nbsp;&nbsp;foreach($cities as $city) &lbrace;<br>
-                &nbsp;&nbsp;&nbsp;$city_is_third_level = (isset($city->parent) && isset(get_term_by('id', $city->parent, 'inwestycje')->parent) && get_term_by('id', $city->parent, 'inwestycje')->parent != 0);<br>
-                &nbsp;&nbsp;&nbsp;if ($city_is_third_level) &lcub;<br>
-                &nbsp;&nbsp;&nbsp;&nbsp;add_rewrite_rule('inwestycje/' . $city->slug, 'index.php/xx', 'top');<br>
-                &nbsp;&nbsp;&rbrace;<br>
-                &nbsp;&rbrace;<br>
-                &rbrace;);
-              `}}
-            ></code>
-          </div>
+          <h2 className="blog-headline-2">What do I do with the leftover url?</h2>
+          <p>As you probably noticed if you're using this to create new urls for your taxonomies then your old url is still working which leaves you with 2 working url that have the same content.</p>
 
-          <p>And this is how you should check for every allowed character in the slug <code>([a-z0-9-_]+)</code></p>
+          <h3 className="blog-headline-3">Backwards Compatibility</h3>
+          <p>If your using this for single posts then you can create 301 redirects in your single-post_type.php. This will allow you to keep your old url still working which means this is the way you will probably do this if you're doing this on a already working website. This will eat a little bit of your google crawl budget which is something you have to know especially when you add this to a big site.</p>
+
+          <h3 className="blog-headline-3">Burn it to the ground</h3>
+          <p>Redirect your old url to 404 and live like it newer existed. You do this by using <code>add_rewrite_rule()</code> and setting your second parameter (your $query) to a page that doesn't exist. Using this will mean your crawl budget won't be affected.</p>
+
         </section>
       </main>
     </>

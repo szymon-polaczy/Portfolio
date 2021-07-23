e9d02e283abb0d7590e658fdb5b2f4fb638e2164
Szymon <thomas_frey@tutanota.com>
2021-07-23 16:35:22 +0200

    Image width and height, Some more keywords and small fixes of texts

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,9 +17,8 @@ const Index = () => {
       <Helmet>
         <meta charSet="utf-8" />
         <title>Get Over Software</title>
-        <meta name="description" content="Get over your software problems with simple solutions, no need to over complicate thins."/>
-        <meta name="keywords" content="Software Problems, Get Over, Get, Over, Website Problems, I need a website, Simple Website, Apps, Applications"/>
-        <link rel="canonical" href="https://get-over.online"/>
+        <meta name="description" content="Get over your software problems with simple and innovative solutions, no need to over complicate things."/>
+        <meta name="keywords" content="Software Problems, Get Over, Get, Over, Website Problems, I need a website, Simple Website, Apps, Applications, Get Over Online Software Company, I need to build a software app, Software Companies, Software Web App Companies, Software Web Applications Companiec"/>
         <link rel="shortcut icon" type="image/jpg" href={favicon}/>
       </Helmet>
       <main className="box-content space-y-64 text-center text-gray-700 uppercase text-4xl">
@@ -28,18 +27,18 @@ const Index = () => {
         <section className="grid xl:grid-cols-2 text-4xl" id="what">
           <article>
             <h2>Whatever the device is</h2>
-            <img src={devices} alt="Phone, Laptop, Big Screen" className="max-h-125 mx-auto"/>
+            <img src={devices} alt="Get Over Online is build with every screen in mind" className="max-h-125 mx-auto" width="845" height="500"/>
           </article>
 
           <article className="mt-24 xl:mt-0">
             <h2>Wherever you are</h2>
-            <img src={world} alt="Las Vegas, Chille, In the middle of the woods" className="max-h-125 mx-auto"/>
+            <img src={world} alt="Get Over Online is operating in all over the world" className="max-h-125 mx-auto" width="670" height="500"/>
           </article>
         </section>
 
         <section>
           <h2>Let's keep your software projects simple</h2>
-          <img src={simple_innovative} alt="Simple and Inovative" className="w-full max-w-screen-lg mx-auto"/>
+          <img src={simple_innovative} alt="Get Over Online is simple and innovative" className="w-full max-w-screen-lg mx-auto" width="1024" height="757"/>
         </section>
 
         <section>
@@ -49,18 +48,18 @@ const Index = () => {
         <section className="grid xl:grid-cols-2">
           <article>
             <h2>Letting you stand out</h2>
-            <img src={stand_out} alt="" className="max-h-125 mx-auto"/>
+            <img src={stand_out} alt="Stand out with Get Over Online" className="max-h-125 mx-auto" width="700" height="500"/>
           </article>
 
           <article className="mt-24 xl:mt-0">
             <h2>And getting your users absorbed</h2>
-            <img src={absorbed} alt="" className="max-h-125 mx-auto"/>
+            <img src={absorbed} alt="Absorb you users with the help of Get Over Online" className="max-h-125 mx-auto" width="582" height="500"/>
           </article>
         </section>
 
         <section className="mb-36 relative w-full lg:w-max mx-auto">
           <h2>let's get in contact</h2>
-          <img src={contact} alt="" className="w-full max-w-screen-lg mx-auto"/>
+          <img src={contact} alt="Contact Get Over Online" className="w-full max-w-screen-lg mx-auto" width="1024" height="555"/>
           <a href="mailto:thomas_frey@tutanota.com" className="md:absolute md:bottom-32 md:right-24 block mt-6 md:mt-0 text-lg sm:text-xl lg:text-2xl xl:text-3xl hover:underline">thomas_frey@tutanota.com</a>
         </section>
       </main>
